071f16eb4c8e337262e3f34e8d547d1d35b11ca3
Paulo Gama <pgama@thoughtworks.com>
2018-11-18 10:50:31 -0200

    Initial Commit

---
+++ Filantropia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Filantropia
+//
+//  Created by Paulo Gama on 18/11/18.
+//  Copyright © 2018 Paulo Gama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
